66d202c1a3bd302e345ee24c7355da8aa432c850
sbmaxx <sbmaxx@yandex-team.ru>
2015-05-29 13:14:26 +0300

    use grunt release

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,6 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-      bump: {
-        options: {
-          files: ['package.json'],
-          updateConfigs: [],
-          commit: true,
-          commitMessage: 'Release v%VERSION%',
-          commitFiles: ['package.json'],
-          createTag: true,
-          tagName: 'v%VERSION%',
-          tagMessage: 'Version %VERSION%',
-          push: true,
-          pushTo: 'upstream',
-          gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-          globalReplace: false,
-          prereleaseName: false,
-          regExp: false
-        }
-      },
-    })
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-bump');
-
+  grunt.loadNpmTasks('grunt-release');
   // Default task(s).
   grunt.registerTask('default', ['bump']);
 
